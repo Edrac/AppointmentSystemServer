8ba0648224419c04d315e676a5b9fba99b49b643
Edison Acuna <edisonricardoac@hotmail.com>
2022-09-17 13:20:26 -0500

    methods updated

--- AppointmentSystem/Controllers/AppointmentsController.cs
+++ AppointmentSystem/Controllers/AppointmentsController.cs
@@ -10,7 +10,7 @@ namespace AppointmentSystem.Controllers
     [ApiController]
     public class AppointmentsController : ControllerBase
     {
-        private AppSystemDbContext _dbContext;
+        private readonly AppSystemDbContext _dbContext;
         public AppointmentsController(AppSystemDbContext dbContext)
         {
             _dbContext = dbContext;
@@ -25,29 +25,68 @@ namespace AppointmentSystem.Controllers
             return Ok(list);
         }
 
-        // GET api/<AppointmentsController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //// GET api/<AppointmentsController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
 
         // POST api/<AppointmentsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Create([FromBody] Appointment appointment)
         {
+            //check if values are present
+            if(appointment == null && appointment?.StartTime == null || appointment?.EndTime == null || appointment?.UserFK == null)
+            {
+                return BadRequest("One or more parameters are missing!");
+            }
+            //check if dates are available
+            var appointments = _dbContext.Appointments.Where(a => a.StartTime == appointment.StartTime || a.EndTime == appointment.EndTime);
+            if (appointments.Any())
+            {
+                return BadRequest(appointment);
+            }
+            else
+            {
+                try
+                {
+                    _dbContext.Appointments.Add(appointment);
+                    _dbContext.SaveChanges();
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, "An error has occurred");
+                }
+            }
         }
 
-        // PUT api/<AppointmentsController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        //// PUT api/<AppointmentsController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
         // DELETE api/<AppointmentsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(Guid id)
         {
+            try
+            {
+                var appointment = _dbContext.Appointments.FirstOrDefault(a => a.Id == id);
+                if (appointment == null)
+                {
+                    return StatusCode(404, "Appointment not found!");
+                }
+                _dbContext.Appointments.Remove(appointment);
+                _dbContext.SaveChanges();
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, "An error has occurred");
+            }
         }
     }
 }
--- AppointmentSystem/Controllers/UsersController.cs
+++ AppointmentSystem/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using AppointmentSystem.Data;
+using AppointmentSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -9,7 +10,7 @@ namespace AppointmentSystem.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        private AppSystemDbContext _dbContext;
+        private readonly AppSystemDbContext _dbContext;
 
         public UsersController(AppSystemDbContext dbContext)
         {
@@ -17,36 +18,45 @@ namespace AppointmentSystem.Controllers
         }
 
 
-        //// GET: api/<UsersController>
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
-
-        // GET api/<UsersController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET: api/<UsersController>
+        [HttpGet]
+        public IActionResult Get()
         {
-            return "value";
+            var list = _dbContext.Users?.ToList();
+            return Ok(list);
         }
+
+        //// GET api/<UsersController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
 
         // POST api/<UsersController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Create([FromBody] User user)
         {
+            if (user == null && user?.Id == "")
+            {
+                return BadRequest("User is lacking one or more attributes");
+            }
+            try
+            {
+                _dbContext.Users.Add(user);
+                _dbContext.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error has occurred");
+            }
         }
 
-        // PUT api/<UsersController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<UsersController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        //// DELETE api/<UsersController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(string id)
+        //{
+        //}
     }
 }
