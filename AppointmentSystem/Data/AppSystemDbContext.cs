0fa93135ac96af56ad1a04d26d97fcf5514e3b12
Edison Acuna <edisonricardoac@hotmail.com>
2022-09-17 12:09:02 -0500

    db context class implemented

---
+++ AppointmentSystem/Data/AppSystemDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AppointmentSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppointmentSystem.Data
+{
+    public class AppSystemDbContext: DbContext
+    {
+        public AppSystemDbContext(DbContextOptions<AppSystemDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+
+    }
+}
